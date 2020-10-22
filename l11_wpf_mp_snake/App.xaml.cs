2ce169b8c1e8d394c981f07fd604d8ca7f259abf
afonin_v@itstep.academy <afonin_v@itstep.academy>
2020-10-22 20:38:22 +0600

    Add project files.

---
+++ l11_wpf_mp_snake/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace l11_wpf_mp_snake
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ l11_wpf_mp_snake/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace l11_wpf_mp_snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int GRID_WIDTH = 32;
+        const int GRID_HEIGHT = 32;
+
+        int[,] gameField = new int[GRID_WIDTH,GRID_HEIGHT];
+        int pos_row = 8;
+        int pos_col = 8;
+
+        enum Direction
+        {
+            Unknown,
+            Left,
+            Right,
+            Up,
+            Down
+        }
+
+        Direction snakeDir = Direction.Left;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var ticker = new DispatcherTimer();
+            ticker.Interval = new TimeSpan( 33300*30 ); //30fps
+            ticker.Tick += UpdateGame;
+            ticker.Start();
+        }
+
+        private void UpdateGame(object sender, EventArgs e)
+        {
+            pos_row -= 1; //up
+
+            Grid.SetRow(snakeHead, pos_row);
+        }
+    }
+}
